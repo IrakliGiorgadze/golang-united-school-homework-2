9d00a887f6438fdd8f511abc553bedb7ac37f92b
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-02-17 21:26:32 +0400

    truncate pi

--- solution.go
+++ solution.go
@@ -1,6 +1,8 @@
 package square
 
-import "math"
+import (
+	"math"
+)
 
 type Num int
 
@@ -10,17 +12,9 @@ const (
 	SidesSquare   Num = 4
 )
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
-
 func CalcSquare(sideLen float64, sidesNum Num) float64 {
 	if sidesNum == SidesCircle {
-		return math.Pi * (sideLen / 2) * (sideLen / 2)
+		return math.Floor(math.Pi*100) / 100 * sideLen * sideLen
 	} else if sidesNum == SidesTriangle {
 		return math.Sqrt(3) / 4 * sideLen * sideLen
 	} else if sidesNum == SidesSquare {
